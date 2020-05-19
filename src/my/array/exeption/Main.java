eb8ab279a5ed77c004231b1d6c03abf427916fb4
IgorBrykin <igorbrikin@gmail.com>
2020-05-19 16:43:37 +0300

    Hello

--- src/my/array/exeption/Main.java
+++ src/my/array/exeption/Main.java
@@ -3,6 +3,6 @@ package my.array.exeption;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(new Hello().toString());
     }
 }
